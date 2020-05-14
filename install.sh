767072d5c9f720e118f9537089739c05b201f92b
Hwanjin Jeong <hwanjin.jeong@gmail.com>
2020-05-14 17:54:31 +0900

    modify install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
+export WORKSPACE=$HOME/src
+mkdir -p $WORKSPACE
+
+# install home brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+brew install ghq ripgrep fzf autojump direnv jenv emacs tmux kubernetes-cli kubectx kustomize skaffold terraform
+
+brew tap homebrew/cask-fonts homebrew/cask-drivers
+brew cask install slack spectacle docker jetbrains-toolbox font-source-code-pro kensington-trackball-works karabiner-elements alacritty alfred google-chrome lens
+
+git clone https://github.com/hwanjin-jeong/.emacs.d $HOME/emacs.d
+git clone https://github.com/hwanjin-jeong/.config $HOME/.config
+
+# zsh configuration
+export ZSH=${HOME}/.config/.oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+ln -s ~/.config/.zshrc ~/
+
+# tmux
 ln -s ~/.config/.tmux.conf ~/
